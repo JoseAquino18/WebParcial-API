f74b068e800000dce5d38b80759ab0c8d20bed89
JoseAquino18 <joseaquin8@gmail.com>
2025-07-04 17:22:57 -0500

    Update Program.cs

--- WebParcial/Program.cs
+++ WebParcial/Program.cs
@@ -58,4 +58,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000"; // Usa "5000" por defecto si no está configurado el puerto
-app.Run();
+app.Run($"http://0.0.0.0:{port}");
